17601ea6da9eea42f6d0cde2d9853ed59f11c36d
djm012 <93957956+djm012@users.noreply.github.com>
2025-01-13 17:08:03 +0800

    Update textvqa_awq.sh

--- scripts/v1_5/eval/textvqa_awq.sh
+++ scripts/v1_5/eval/textvqa_awq.sh
@@ -1,10 +1,11 @@
-python -m llava.eval.model_awq_loader \
-    --model-path ./checkpoints/awq_model/ \
+python -m llava.eval.model_vqa_awq_llava \
+    --model-path liuhaotian/llava-v1.5-13b \
     --question-file ./playground/data/eval/textvqa/llava_textvqa_val_v051_ocr.jsonl \
     --image-folder ./playground/data/eval/textvqa/test_images \
     --answers-file ./playground/data/eval/textvqa/answers/llava-v1.5-13b-awq.jsonl \
-    --temperature 0 \
+    --temperature 0.7 \
     --conv-mode vicuna_v1
+    # --projector-path ./checkpoints/llava-v1.5-13b-pretrain/ \
 
 python -m llava.eval.eval_textvqa \
     --annotation-file ./playground/data/eval/textvqa/TextVQA_0.5.1_val.json \
